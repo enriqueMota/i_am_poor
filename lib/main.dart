1bee0013f79ad76bfabe9008c0f49fd518efeb29
Enrique mota <enrique.motadejesus@gmail.com>
2024-12-04 15:19:51 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: const Text('I Am Poor :\'('),
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://img.freepik.com/free-vector/homeless-man-sitting-ground-flat-vector-illustration-desperate-hungry-poor-male-person-sitting-street-near-trash-bin-asking-help-getting-into-financial-trouble-poverty-concept_74855-23087.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
